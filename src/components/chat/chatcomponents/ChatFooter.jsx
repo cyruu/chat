d1b73847b9155be5d37f15c13f756f137ca001e6
cyruu <cyruz.mhr09@gmail.com>
2024-04-24 11:29:34 +0545

    real

--- src/components/chat/chatcomponents/ChatFooter.jsx
+++ src/components/chat/chatcomponents/ChatFooter.jsx
@@ -1,7 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { firebaseServices } from "../../../index";
+import { useDispatch } from "react-redux";
+import { getDocs, query, collection, where } from "firebase/firestore";
+import { db } from "../../../firebase/firebaseConfig";
+import { setAllMessages } from "../../../redux/slice";
 function ChatFooter({ sentBy, sentTo }) {
   const [message, setMessage] = useState("");
+  const dis = useDispatch();
+  const getMessages = async () => {
+    const allMessages = [];
+    // sent by cyrus@gmail.com and received by another user
+    const sentQuery = query(
+      collection(db, "messages"),
+      //sent by cyrus@gmail.com
+      where("sentBy", "==", sentBy),
+      where("sentTo", "==", sentTo)
+    );
+    const sentSnapshot = await getDocs(sentQuery);
+    sentSnapshot.forEach((doc) => {
+      const user = doc.data();
+      allMessages.push(user);
+    });
+    // sent by others and received by cyrus@gmail.com
+    const receivedQuery = query(
+      collection(db, "messages"),
+      //sent by cyrus@gmail.com
+      where("sentBy", "==", sentTo),
+      where("sentTo", "==", sentBy)
+    );
+    const receivedSnapshot = await getDocs(receivedQuery);
+    receivedSnapshot.forEach((doc) => {
+      const user = doc.data();
+      allMessages.push(user);
+    });
+
+    // arrange message in order
+    allMessages.forEach((obj) => {
+      obj.sentTime = new Date(obj.sentTime);
+    });
+
+    allMessages.sort((a, b) => {
+      return a.sentTime - b.sentTime;
+    });
+    //conver date back to string
+    allMessages.forEach((obj) => {
+      obj.sentTime = new Date(obj.sentTime).toISOString();
+    });
+
+    //send data to slice state
+    dis(setAllMessages({ allMessages }));
+  };
+
   const handleSendMessage = (e) => {
     e.preventDefault();
     if (message != "") {
@@ -14,6 +63,8 @@ function ChatFooter({ sentBy, sentTo }) {
       };
       firebaseServices.sendMessage(msg);
       setMessage("");
+      console.log("ADDED");
+      getMessages();
     } else {
       console.log("empty");
     }
